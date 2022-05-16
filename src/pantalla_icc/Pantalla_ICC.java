f1ea7d10eb6fa066193b222c26e30f2990e7a41c
Luciano <lnicolas15@alumnos.utalca>
2022-05-15 22:54:22 -0400

    Open video and play
    Abre el video y lo reproduce automaticamente

--- src/pantalla_icc/Pantalla_ICC.java
+++ src/pantalla_icc/Pantalla_ICC.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package pantalla_icc;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,13 +20,15 @@ public class Pantalla_ICC {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("asd");
+    public static void main(String[] args) throws AWTException {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\lib\\chromedriver.exe");
         
         WebDriver driver = new ChromeDriver();
+        Robot robot = new Robot();
         
-        driver.navigate().to("https://google.com/");
+        driver.navigate().to("https://www.canva.com/design/DAFAOF4JrLA/c_lvXhwMRt2DrP30c-PtnQ/watch?utm_content=DAFAOF4JrLA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink");
+        robot.keyPress(KeyEvent.VK_SPACE);
+        robot.keyRelease(KeyEvent.VK_SPACE);
     }
     
     
